9e0fc26384b7037ae9ceb163ba81ddf3c1b335cb
Zireael07 <zi@mail.com>
2016-09-09 19:47:19 +0200

    classes responsible for drawing the game map

---
+++ class/Cell.lua
@@ -0,0 +1,116 @@
+require 'T-Engine.class'
+
+module("Cell", package.seeall, class.make)
+
+SideType = { Empty=1, Wall=2, Door=3 }
+
+function Cell:init()
+    --side detection
+    self.eastSide = SideType.Wall
+    self.northSide = SideType.Wall
+    self.southSide = SideType.Wall
+    self.westSide = SideType.Wall
+    self.visited = false
+    self.isCorridor = false
+
+    --see what we contain
+    self.terrain = nil
+    self.actor = nil
+    self.object = nil
+end
+
+function Cell:CalculateDeadEndCorridorDirection()
+    if (not self:getIsDeadEnd()) then print('ERROR: InvalidOperationException (Cell:CalculateDeadEndCorridorDirection): not getIsDeadEnd()') end
+
+    if (self.northSide == SideType.Empty) then return DirectionType.North end
+    if (self.southSide == SideType.Empty) then return DirectionType.South end
+    if (self.westSide == SideType.Empty) then return DirectionType.West end
+    if (self.eastSide == SideType.Empty) then return DirectionType.East end
+
+    print('ERROR: InvalidOperationException (Cell:CalculateDeadEndCorridorDirection)')
+end
+
+------------------------------------------------------
+-- helper functions
+------------------------------------------------------
+function Cell:getVisited()
+    return self.visited
+end
+function Cell:setVisited( visited )
+    self.visited = visited
+end
+
+function Cell:getNorthSide()
+    return self.northSide
+end
+function Cell:setNorthSide( northSide )
+    self.northSide = northSide
+end
+
+function Cell:getSouthSide()
+    return self.southSide
+end
+function Cell:setSouthSide( southSide )
+    self.southSide = southSide
+end
+
+function Cell:getEastSide()
+    return self.eastSide
+end
+function Cell:setEastSide( eastSide )
+    self.eastSide = eastSide
+end
+
+function Cell:getWestSide()
+    return self.westSide
+end
+function Cell:setWestSide( westSide )
+    self.westSide = westSide
+end
+
+function Cell:getIsDeadEnd()
+--  print("Wallcount=", self:getWallCount() )
+    return self:getWallCount() == 3
+end
+
+function Cell:getIsCorridor()
+    return self.isCorridor
+end
+function Cell:setIsCorridor( isCorridor )
+    self.isCorridor = isCorridor
+end
+
+function Cell:getWallCount()
+    local wallCount = 0
+    if (self.northSide == SideType.Wall) then wallCount=wallCount+1 end
+    if (self.southSide == SideType.Wall) then wallCount=wallCount+1 end
+    if (self.westSide == SideType.Wall) then wallCount=wallCount+1 end
+    if (self.eastSide == SideType.Wall) then wallCount=wallCount+1 end
+    return wallCount
+end
+
+function Cell:getTerrain()
+    return self.terrain
+end
+
+function Cell:setTerrain(val)
+    self.terrain = val
+end
+
+function Cell:getActor()
+    return self.actor
+end
+
+function Cell:setActor(val)
+    self.actor = val
+end
+
+function Cell:getObject()
+    return self.object
+end
+
+function Cell:setObject(val)
+    self.object = val
+end
+
+return Cell
---
+++ class/Map.lua
@@ -0,0 +1,190 @@
+require 'T-Engine.class'
+
+local Cell = require 'class.Cell'
+
+module("Map", package.seeall, class.make)
+
+function _M:init(width, height)
+    print('Map:initialize', width, height)
+    
+    self.cells = {}
+    self.bounds = { X=0, Y=0, Width=width, Height=height }
+
+    -- Initialize the array of cells
+    for x = 0, self.bounds.Width-1 do
+        self.cells[x] = {}
+        for y = 0, self.bounds.Height-1 do
+            self.cells[x][y] = Cell:new()
+        end
+    end
+end
+
+
+function Map:loadTiles()
+    floor = love.graphics.newImage("gfx/tiles/terrain/floor.png")
+    wall = love.graphics.newImage("gfx/tiles/terrain/wall.png")
+    floor_bright = love.graphics.newImage("gfx/tiles/terrain/floor_bright.png")
+    player_tile = love.graphics.newImage("gfx/player/racial_dolls/human_m.png")
+end
+
+function Map:setupMapView()
+   --???
+   map_w = 2
+   map_h = 2
+   --scrolling offset
+   map_x = 0
+   map_y = 0
+   --where to draw
+   map_offset_x = 120
+   map_offset_y = 0
+   --size in cells
+   map_display_w = Map:getWidth()-1
+   map_display_h = Map:getHeight()-1
+ 
+   --tile size
+   tile_w = 32
+   tile_h = 32
+end
+
+function Map:getBounds()
+--  print('Map:getBounds')
+    return self.bounds
+end
+
+-- renamed functions!!
+function Map:getCell(x, y)
+    --print('Map:getCell', x, y)
+    if x > self.bounds.Width - 1 or x < 0 then 
+    --  print("ERROR: Tried to get cell of "..x.." which is outside bounds!")
+      return end
+
+    if y > self.bounds.Height-1 or y < 0 then
+    --  print("ERROR: Tried to get cell of "..y.."which is outside bounds!")
+      return end
+
+    if not self.cells[x][y] then
+    --  print("ERROR: Tried to get cell of "..x..","..y.."but no such cell!") 
+      return end
+    return self.cells[x][y]
+end
+
+--defaults to setting terrain
+function Map:setCell(x, y, value)
+   -- print('Map:setCell :', x, y, value)
+    self.cells[x][y]:setTerrain(value)
+end
+
+function Map:setCellActor(x, y, value)
+  --print("Map:setCellActor: ", x, y, value)
+  self.cells[x][y]:setActor(value)
+end
+
+function Map:setCellObject(x, y, value)
+  print("Map:setCellObject: ", x, y, value)
+  self.cells[x][y]:setObject(value)
+end
+
+--getters
+function Map:getCellTerrain(x, y)
+  local res
+  if not Map:getCell(x,y) then return nil 
+  else 
+    local cell = Map:getCell(x,y)
+    res = cell:getTerrain()
+   -- print("Terrain for cell: "..x.." "..y.." is..", res)
+    return res
+    end
+end 
+
+function Map:getCellActor(x,y)
+   local res
+  if not Map:getCell(x,y) then return nil 
+  else 
+    local cell = Map:getCell(x,y)
+    res = cell:getActor()
+    --if res then print("Actor for cell: "..x.." "..y.." is..", res) end
+    return res
+    end
+end
+
+function Map:getCellObject(x,y)
+   local res
+  if not Map:getCell(x,y) then return nil 
+  else 
+    local cell = Map:getCell(x,y)
+    res = cell:getObject()
+    --if res then print("Object for cell: "..x.." "..y.." is..", res) end
+    return res
+    end
+end
+
+--bounds
+function Map:getWidth()
+--  print('Map:getWidth')
+    return self.bounds.Width
+end
+function Map:getHeight()
+--  print('Map:getHeight')
+    return self.bounds.Height
+end
+
+--convert terrain symbols to tiles
+function Map:convertTerraintoTile(x, y)
+  if not Map:getCellTerrain(x,y) then tile = floor_bright 
+  else
+    local string = Map:getCellTerrain(x, y)
+      if string == "." then tile = floor end
+      if string == "#" then tile = wall end
+    end
+    
+    --print("Tile gotten for x: "..x.."y: "..y)
+    return tile
+end
+
+--assume we already checked if there is an actor at x,y
+function Map:convertActortoTile(x,y)
+  local string = Map:getCellActor(x,y)
+  if string == "player_tile" then tile = player_tile end
+  if string == "orc" then tile = orc end
+  --print ("Actor tile gotten for x: "..x.."y: "..y)
+  return tile
+end
+
+
+
+--assume we already checked if there is an actor at x,y
+function Map:convertObjecttoTile(x,y)
+  local string = Map:getCellObject(x,y)
+  if string == "longsword" then tile = longsword end
+  --print ("Object tile gotten for x: "..x.."y: "..y)
+  return tile
+end
+
+function Map:display()
+  for y=0, Map:getWidth()-1 do
+      for x=0, Map:getHeight()-1 do                                                      
+         --print("Querying: ", x, map_x, y, map_y)
+         --draw terrain
+         love.graphics.draw(
+            Map:convertTerraintoTile(x+map_x, y+map_y),
+            (x*tile_w)+map_offset_x, 
+            (y*tile_h)+map_offset_y)
+         --check if we have any objects to draw
+         if Map:getCellObject(x,y) then
+            --if yes then draw
+            love.graphics.draw(
+              Map:convertObjecttoTile(x+map_x, y+map_y),
+              (x*tile_w)+map_offset_x, 
+              (y*tile_h)+map_offset_y)
+          end
+         --check if we have any actors to draw
+         if Map:getCellActor(x,y) then
+            --if yes then draw
+            love.graphics.draw(
+              Map:convertActortoTile(x+map_x, y+map_y),
+              (x*tile_w)+map_offset_x, 
+              (y*tile_h)+map_offset_y)
+          end
+      end
+   end
+end
