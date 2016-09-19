e61823a9cf3f257c13589fcd996892deab2e0ed7
Zireael07 <zi@mail.com>
2016-09-19 19:22:37 +0200

    some useful functions

--- utils.lua
+++ utils.lua
@@ -17,4 +17,19 @@ function utils:distance(sx, sy, tx, ty)
   return math.max( sx-tx, tx-ty )
 end
 
+function table.append(dst, src)
+    for i = 1, #src do dst[#dst+1] = src[i] end
+    return dst
+end
+
+function string.capitalize(str)
+    if #str > 1 then
+        return string.upper(str:sub(1, 1))..str:sub(2)
+    elseif #str == 1 then
+        return str:upper()
+    else
+        return str
+    end
+end
+
 return utils
