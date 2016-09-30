690d4d2552495a1b03633de97a920bcd828a3a9d
Zireael07 <zi@mail.com>
2016-09-30 18:24:16 +0200

    yaay implement ability arrays

--- interface/ActorStats.lua
+++ interface/ActorStats.lua
@@ -18,8 +18,11 @@ function ActorStats:init(t)
   for _, s in ipairs(ActorStats.stats) do
     self.stats[s] = { current = 10 }
     --print("Actor: stat "..s.." set to ", self.stats[s].current)
-    
   end
+
+  local array = self:setArray()
+  self:applyArray(array)
+
   print_to_log("Actor: initiated stats for ", self.name)
 end
 
@@ -31,6 +34,68 @@ function ActorStats:getStat(s)
     val = stat.current
     --print("Value is", val)
     return val
+end
+
+function ActorStats:setStat(s, val)
+  local stat = self.stats[s]
+
+  stat.current = val
+  print("Set stat", s, "to", val)
+end
+
+
+function ActorStats:setArray(val)
+  local array = {}
+  if val == "heroic" then
+    array = { 15, 14, 13, 12, 10, 8}
+  else
+    array = { 13, 12, 11, 10, 9, 8 }
+  end
+
+  return array
+end
+
+function ActorStats:applyArray(array, str)
+
+  if str == "ranged" then
+    print("Using ranged array")
+    self:setStat("STR", array[3])
+    self:setStat("DEX", array[1])
+    self:setStat("CON", array[2])
+    self:setStat("INT", array[4])
+    self:setStat("WIS", array[5])
+    self:setStat("CHA", array[6])
+  elseif str == "divine" then
+    self:setStat("STR", array[4])
+    self:setStat("DEX", array[6])
+    self:setStat("CON", array[2])
+    self:setStat("INT", array[5])
+    self:setStat("WIS", array[1])
+    self:setStat("CHA", array[3])
+  elseif str == "arcane" then
+    self:setStat("STR", array[6])
+    self:setStat("DEX", array[2])
+    self:setStat("CON", array[4])
+    self:setStat("INT", array[1])
+    self:setStat("WIS", array[5])
+    self:setStat("CHA", array[3])
+  elseif str == "skill" then
+    self:setStat("STR", array[4])
+    self:setStat("DEX", array[2])
+    self:setStat("CON", array[3])
+    self:setStat("INT", array[1])
+    self:setStat("WIS", array[6])
+    self:setStat("CHA", array[5])
+  else 
+    --bog-standard melee
+    print("Using melee array")
+    self:setStat("STR", array[1])
+    self:setStat("DEX", array[3])
+    self:setStat("CON", array[2])
+    self:setStat("INT", array[5])
+    self:setStat("WIS", array[4])
+    self:setStat("CHA", array[6])
+  end
 end
 
 return ActorStats
