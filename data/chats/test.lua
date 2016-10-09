d2b60823f136b4978e1eb08634522e15b98b228d
Zireael07 <zi@mail.com>
2016-10-09 20:31:02 +0200

    fix stat checks in test convo

--- data/chats/test.lua
+++ data/chats/test.lua
@@ -15,7 +15,7 @@ newChat{ id="start",
     else return "sorry" end
     end,
     cond=function(npc, player)
-        if player:getInt() < 10 then return end
+        if player:getStat("INT") < 10 then return end
         return true
     end
     },
@@ -24,7 +24,7 @@ newChat{ id="start",
     else return "sorry" end
     end,
     cond=function(npc, player)
-        if player:getInt() >= 10 then return end
+        if player:getStat("INT") >= 10 then return end
         return true
     end
     },
@@ -33,7 +33,7 @@ newChat{ id="start",
     else return "sorry" end
     end,
     cond=function(npc, player)
-        if player:getInt() < 10 then return end
+        if player:getStat("INT") < 10 then return end
         return true
     end
     },
@@ -42,7 +42,7 @@ newChat{ id="start",
     else return "sorry" end
     end,
     cond=function(npc, player)
-        if player:getInt() >= 10 then return end
+        if player:getStat("INT") >= 10 then return end
         return true
     end
     },
