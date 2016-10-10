0e875491383fe8af296a0a27ac649369c3c6a367
Zireael07 <zi@mail.com>
2016-10-10 12:11:24 +0200

    load portrait elements

--- class/Tile.lua
+++ class/Tile.lua
@@ -56,6 +56,63 @@ function Tile:loadTiles()
     shoulder_inv = lg.newImage("gfx/inventory/shoulder_inv.png"),
     tool_inv = lg.newImage("gfx/inventory/tool_inv.png"),
 
+
+    --portraits
+    doll = lg.newImage("gfx/portraits/base.png"),
+    doll_drow = lg.newImage("gfx/portraits/base_drow.png"),
+    doll_dwarf = lg.newImage("gfx/portraits/ozy/dwarf_base.png"),
+    doll_dwarf_alt = lg.newImage("gfx/portraits/ozy/dwarf_base2.png"),
+
+    drow_hair_1 = lg.newImage("gfx/portraits/ozy/drow_hair1.png"),
+    drow_hair_2 = lg.newImage("gfx/portraits/ozy/drow_hair2.png"),
+    drow_hair_3 = lg.newImage("gfx/portraits/ozy/drow_hair3.png"),
+    drow_hair_4 = lg.newImage("gfx/portraits/ozy/drow_hair4.png"),
+
+    hair_black = lg.newImage("gfx/portraits/ozy/hair_black.png"),
+    hair_black2 = lg.newImage("gfx/portraits/ozy/hair_black2.png"),
+    hair_brown = lg.newImage("gfx/portraits/ozy/hair_brown.png"),
+    hair_gray = lg.newImage("gfx/portraits/ozy/hair_gray.png"),
+    hair_red = lg.newImage("gfx/portraits/ozy/hair_red.png"),
+    hair_white = lg.newImage("gfx/portraits/ozy/hair_white.png"),
+
+    eyebrows_drow = lg.newImage("gfx/portraits/eyebrows_drow.png"),
+    eyebrows_black = lg.newImage("gfx/portraits/ozy/eyebrows_black.png"),
+    eyebrows_black2 = lg.newImage("gfx/portraits/ozy/eyebrows_black2.png"),
+    eyebrows_brown = lg.newImage("gfx/portraits/ozy/eyebrows_brown.png"),
+    eyebrows_gray = lg.newImage("gfx/portraits/ozy/eyebrows_gray.png"),
+    eyebrows_red = lg.newImage("gfx/portraits/ozy/eyebrows_red.png"),
+    eyebrows_white = lg.newImage("gfx/portraits/ozy/eyebrows_white.png"),
+
+    eyes_dwarf_1 = lg.newImage("gfx/portraits/ozy/eyes_dwarf1.png"),
+    eyes_dwarf_2 = lg.newImage("gfx/portraits/ozy/eyes_dwarf2.png"),
+    eyes_dwarf_3 = lg.newImage("gfx/portraits/ozy/eyes_dwarf3.png"),
+    eyes_dwarf_4 = lg.newImage("gfx/portraits/ozy/eyes_dwarf4.png"),
+
+    eyes_amber = lg.newImage("gfx/portraits/eyes_amber.png"),
+    eyes_seablue = lg.newImage("gfx/portraits/eyes_seablue.png"),
+    eyes_seagreen = lg.newImage("gfx/portraits/eyes_seagreen.png"),
+    eyes_yellow = lg.newImage("gfx/portraits/eyes_yellow.png"),
+    eyes_green = lg.newImage("gfx/portraits/eyes_green.png"),
+    eyes_blue = lg.newImage("gfx/portraits/eyes_blue.png"),
+    eyes_gray = lg.newImage("gfx/portraits/eyes_gray.png"),
+    eyes_black = lg.newImage("gfx/portraits/eyes_black.png"),
+    eyes_brown = lg.newImage("gfx/portraits/eyes_brown.png"),
+    eyes_red = lg.newImage("gfx/portraits/eyes_red.png"),
+    eyes_pink = lg.newImage("gfx/portraits/eyes_pink.png"),
+
+    dwarf_nose = lg.newImage("gfx/portraits/ozy/dwarf_nose.png"),
+
+    drow_mouth = lg.newImage("gfx/portraits/ozy/drow_mouth.png"),
+    drow_mouth2 = lg.newImage("gfx/portraits/ozy/drow_mouth2.png"),
+    dwarf_mouth = lg.newImage("gfx/portraits/ozy/dwarf_mouth.png"),
+    dwarf_mouth2 = lg.newImage("gfx/portraits/ozy/dwarf_mouth2.png"),
+    mouth = lg.newImage("gfx/portraits/mouth.png"),
+    mouth2 = lg.newImage("gfx/portraits/mouth2.png"),
+
+    dwarf_beard_black = lg.newImage("gfx/portraits/ozy/dwarf_beard_black.png"),
+    dwarf_beard_brown = lg.newImage("gfx/portraits/ozy/dwarf_beard_brown.png"),
+    dwarf_beard_gray = lg.newImage("gfx/portraits/ozy/dwarf_beard_gray.png"),
+    dwarf_beard_white = lg.newImage("gfx/portraits/ozy/dwarf_beard_white.png"),
   }
   
   
