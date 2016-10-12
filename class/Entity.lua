921ef85c5d13533fb078faa25fac3296f005d6ce
Zireael07 <zi@mail.com>
2016-10-12 13:29:55 +0200

    enable specifying a base containing shared values for several entities

--- class/Entity.lua
+++ class/Entity.lua
@@ -1,7 +1,11 @@
 require 'T-Engine.class'
 
+local utils = require 'utils'
+
 module("Entity", package.seeall, class.make)
 
+--For NPCs now and maybe objects in the future
+--Registers with the table the scheduler is based on
 function Entity:addEntity(e)
     if not e then print("No entity")
     else
@@ -10,6 +14,33 @@ function Entity:addEntity(e)
     return e
     
     end
+end
+
+--Common functions
+function Entity:importBase(t, base)
+    print("[ENTITY] Importing base:",base, "for", t)
+    local temp = table.clone(base, true)
+    table.mergeAppendArray(temp, t, true)
+    t = temp
+    t.base = nil
+    return t
+end
+
+function Entity:newEntity(t, typ)
+    print("[ENTITY] Creating new entity, type:", typ)
+    -- Do we inherit things ?
+    if t.base then
+        local base
+        local b = t.base
+        print("[ENTITY] base is", b)
+        if typ == "actor" then
+            base = npc_types[b]
+        end
+        print("[ENTITY] Creating new entity with a base")
+        t = self:importBase(t, base) 
+    end
+
+    return t
 end
 
 return Entity
--- utils.lua
+++ utils.lua
@@ -22,6 +22,79 @@ function table.append(dst, src)
     return dst
 end
 
+function table.clone(tbl, deep, k_skip)
+    if not tbl then return nil end
+    local n = {}
+    k_skip = k_skip or {}
+    for k, e in pairs(tbl) do
+        if not k_skip[k] then
+            -- Deep copy subtables, but not objects!
+            if deep and type(e) == "table" and not e.__ATOMIC and not e.__CLASSNAME then
+                n[k] = table.clone(e, true, k_skip)
+            else
+                n[k] = e
+            end
+        end
+    end
+    return n
+end
+
+table.NIL_MERGE = {}
+
+--- Merges two tables in-place.
+-- The table.NIL_MERGE is a special value that will nil out the corresponding dst key.
+-- @param dst The destination table, which will have all merged values.
+-- @param src The source table, supplying values to be merged.
+-- @param deep Boolean that determines if tables will be recursively merged.
+-- @param k_skip A table containing key values set to true if you want to skip them.
+-- @param k_skip_deep Like k_skip, except this table is passed on to the deep recursions.
+-- @param addnumbers Boolean that determines if two numbers will be added rather than replaced.
+function table.merge(dst, src, deep, k_skip, k_skip_deep, addnumbers)
+    k_skip = k_skip or {}
+    k_skip_deep = k_skip_deep or {}
+    for k, e in pairs(src) do
+        if not k_skip[k] and not k_skip_deep[k] then
+            -- Recursively merge tables
+            if deep and dst[k] and type(e) == "table" and type(dst[k]) == "table" and not e.__ATOMIC and not e.__CLASSNAME then
+                table.merge(dst[k], e, deep, nil, k_skip_deep, addnumbers)
+            -- Clone tables if into the destination
+            elseif deep and not dst[k] and type(e) == "table" and not e.__ATOMIC and not e.__CLASSNAME then
+                dst[k] = table.clone(e, deep, nil, k_skip_deep)
+            -- Nil out any NIL_MERGE entries
+            elseif e == table.NIL_MERGE then
+                dst[k] = nil
+            -- Add number entries if "add" is set
+            elseif addnumbers and not dst.__no_merge_add and dst[k] and type(dst[k]) == "number" and type(e) == "number" then
+                dst[k] = dst[k] + e
+            -- Or simply replace/set with the src value
+            else
+                dst[k] = e
+            end
+        end
+    end
+    return dst
+end
+
+function table.mergeAppendArray(dst, src, deep, k_skip, k_skip_deep, addnumbers)
+    -- Append the array part
+    k_skip = k_skip or {}
+    for i = 1, #src do
+        k_skip[i] = true
+        local b = src[i]
+        if deep and type(b) == "table" then
+            if b.__ATOMIC or b.__CLASSNAME then
+                b = b:clone()
+            else
+                b = table.clone(b, true)
+            end
+        end
+        table.insert(dst, b)
+    end
+    -- Copy the table part
+    return table.merge(dst, src, deep, k_skip, k_skip_deep, addnumbers)
+end
+
+
 function string.capitalize(str)
     if #str > 1 then
         return string.upper(str:sub(1, 1))..str:sub(2)
