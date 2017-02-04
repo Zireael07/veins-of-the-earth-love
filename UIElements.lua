d6b8e8c5b81a2e736e8b88203cd229b2287d88a6
Zireael07 <zi@mail.com>
2017-02-04 19:13:25 +0100

    define an image button, too

--- UIElements.lua
+++ UIElements.lua
@@ -16,37 +16,51 @@ function UIElements:init_text_button(x,y, w, id, text, on_press)
     element[#element+1] = {x = x, y=y, w=w, id=id, text=text, on_press=on_press}
 end
 
---function UIElements:init_
+function UIElements:init_image_button(x,y, id, image, param, on_press)
+    if not x or not y or not id or not image then print("[UI] Missing parameters!") return end
+    element[#element+1] = {x=x, y=y, id=id, image=image, param=param, on_press=on_press}
+end
 
 --generic stuff
 function UIElements:draw(id)
     for i, e in ipairs(element) do
-        if id == e.id then
-            love.graphics.setColor(colors.RED)
-        else
-            love.graphics.setColor(255, 255, 102)
+        if e.text then
+            if id == e.id then
+                love.graphics.setColor(colors.RED)
+            else
+                love.graphics.setColor(255, 255, 102)
+            end
+            love.graphics.print(e.text, e.x, e.y)
         end
-        love.graphics.print(e.text, e.x, e.y)
+        if e.image then
+            love.graphics.draw(loaded_tiles[e.image], e.x, e.y)
+        end
     end
 end
 
 function UIElements:mouse()
     local id
+    local param
     for i,e in ipairs(element) do
-        if mouse.x > e.x and mouse.x < e.x + e.w then
+        if mouse.x > e.x and mouse.x < e.x + (e.w or 20) then
             if mouse.y > e.y and mouse.y < e.y + 20 then
-                id = e.id
+                if e.id then
+                    id = e.id
+                end
+                if e.param then
+                    param = e.param
+                end
                 --print("We're over element "..id)
             end
         end
     end
-    return id
+    return id, param
 end
 
 function UIElements:mouse_pressed(x,y,b)
     if b == 1 then
         for i,e in ipairs(element) do
-            if x > e.x and x < e.x + e.w then
+            if x > e.x and x < e.x + (e.w or 20) then
                 if y > e.y and y < e.y + 20 then
                     --print("Pressed mouse over element "..e.id)
                     if e.on_press then
@@ -57,6 +71,10 @@ function UIElements:mouse_pressed(x,y,b)
             end
         end
     end
+end
+
+function UIElements:getParameters(i)
+    return element[i].param
 end
 
 return UIElements
--- dialogs/TestDialog.lua
+++ dialogs/TestDialog.lua
@@ -8,6 +8,7 @@ module("TestDialog", package.seeall, class.make)
 function TestDialog:load()
     UI:init_text_button(210, 120, 60, "dummy", "Dummy button", function() print("Clicked dummy!") end)
     UI:init_text_button(210, 140, 20, "blah", "Blah", function() setDialog("help_controls") end)
+    UI:init_image_button(210, 160, "val", "ui_left_arrow", "minus", function() print("Value change is "..(UI:getParameters(3) or " nil")) end)
 end
 
 function TestDialog:draw()
