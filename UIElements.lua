501b27f695bc01115e6111cc7af9fd20324f260e
Zireael07 <zi@mail.com>
2017-02-04 14:13:18 +0100

    testing a new way of setting up UI elements

---
+++ UIElements.lua
@@ -0,0 +1,58 @@
+require 'T-Engine.class'
+
+module("UIElements", package.seeall, class.make)
+
+element = {}
+
+function UIElements:load()
+end
+
+function UIElements:init_text_button(x,y, w, id, text, on_press)
+    if not x or not y or not id or not text then print("[UI] Missing parameters!") return end
+    element[#element+1] = {x = x, y=y, id=id, text=text, on_press=on_press}
+end
+
+--function UIElements:init_
+
+--generic stuff
+function UIElements:draw(id)
+    for i, e in ipairs(element) do
+        if id == e.id then
+            love.graphics.setColor(colors.RED)
+        else
+            love.graphics.setColor(255, 255, 102)
+        end
+        love.graphics.print(e.text, e.x, e.y)
+    end
+end
+
+function UIElements:mouse()
+    local id
+    for i,e in ipairs(element) do
+        if mouse.x > e.x then
+            if mouse.y > e.y and mouse.y < e.y + 20 then
+                id = e.id
+                --print("We're over element "..id)
+            end
+        end
+    end
+    return id
+end
+
+function UIElements:mouse_pressed(x,y,b)
+    if b == 1 then
+        for i,e in ipairs(element) do
+            if x > e.x then
+                if y > e.y and y < e.y + 20 then
+                    print("Pressed mouse over element "..e.id)
+                    if e.on_press then
+                        print("We have on_press, calling it")
+                        e.on_press()
+                    end
+                end
+            end
+        end
+    end
+end
+
+return UIElements
---
+++ dialogs/TestDialog.lua
@@ -0,0 +1,29 @@
+require 'T-Engine.class'
+
+local UI = require "UIElements"
+
+module("TestDialog", package.seeall, class.make)
+
+
+function TestDialog:load()
+    UI:init_text_button(210, 120, 60, "dummy", "Dummy button", function() print("Clicked dummy!") end)
+    UI:init_text_button(210, 140, 20, "blah", "Blah", function() setDialog("help_controls") end)
+end
+
+function TestDialog:draw()
+    love.graphics.setColor(255,255,255)
+    --background
+    love.graphics.draw(loaded_tiles["stone_bg"], 200, 100)
+
+    UI:draw(id)
+end
+
+function TestDialog:mouse()
+    id = UI:mouse()
+end
+
+function TestDialog:mouse_pressed(x,y,b)
+    UI:mouse_pressed(x,y,b)
+end
+
+return TestDialog
